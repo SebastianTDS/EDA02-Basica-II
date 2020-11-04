76707dfd89fde41133f0553c96fcad21a491c41a
Sebastian Da Silva <sebastianjds.99@gmail.com>
2020-11-03 22:52:53 -0300

    Proyecto base EDA02

---
+++ test/unlam/pb2/eda02/TestSistema.java
@@ -0,0 +1,14 @@
+package unlam.pb2.eda02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSistema {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
